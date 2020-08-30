e8f707134569b26c785c6e258c3ddd160fd43886
Łukasz Gretka <lukasz.gretka@gmail.com>
2020-08-30 15:33:18 +0200

    Add project files.

---
+++ Online Bookstore ASP.NET Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Online_Bookstore_ASP.NET_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
